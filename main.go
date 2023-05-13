3a4140bd00c8d8cfdb86ddbdf4b4bca8c1f351cf
Eric Julianto <algonacci1@gmail.com>
2023-05-13 22:26:52 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,81 +2,62 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"cloud.google.com/go/firestore"
-	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/option"
 )
 
-type SensorData struct {
-	TDS       float64   `json:"tds"`
-	Moisture  float64   `json:"moisture"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+type Data struct {
+	Moisture  float64 `json:"moisture"`
+	TDS       float64 `json:"tds"`
+	PH        float64 `json:"pH"`
+	UpdatedAt string  `json:"updatedAt"`
+	UserID    string  `json:"user_id"`
+	DeviceID  int     `json:"device_id"`
 }
 
 func main() {
-	router := gin.Default()
-
-	opt := option.WithCredentialsFile("serviceAccount.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, "demoandroidmarketplace", option.WithCredentialsFile("serviceAccount.json"))
 	if err != nil {
-		log.Fatalf("Failed to initialize Firebase app: %v", err)
+		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	client, err := app.Firestore(context.Background())
-	if err != nil {
-		log.Fatalf("Failed to initialize Firebase client: %v", client)
-	}
+	r := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "Success fetching the API!", "statusCode": 200})
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
 	})
 
-	router.POST("/data", func(c *gin.Context) {
-		handleSensorData(c, client)
+	r.POST("/data", func(c *gin.Context) {
+		var data Data
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		data.UpdatedAt = time.Now().UTC().String()
+
+		filteredData := Data{
+			Moisture:  data.Moisture,
+			TDS:       data.TDS,
+			PH:        data.PH,
+			UpdatedAt: data.UpdatedAt,
+		}
+
+		_, err := client.Collection("users").Doc(data.UserID).Collection("devices").Doc(strconv.Itoa(data.DeviceID)).Set(ctx, filteredData)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to store data"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Data received and stored successfully"})
 	})
 
-	router.Run(":8080")
-}
-
-func handleSensorData(c *gin.Context, client *firestore.Client) {
-	var data SensorData
-
-	if err := c.ShouldBindJSON(&data); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	fmt.Println("Received sensor data: ")
-	fmt.Println("TDS: ", data.TDS)
-	fmt.Println("Moisture: ", data.Moisture)
-
-	now := time.Now()
-	data.CreatedAt = now
-	data.UpdatedAt = now
-
-	userID := "s8uiODzNahcLoMwc27LNH2EkWTt1"
-
-	// Create a reference to the user's document in the "users" collection
-	userRef := client.Collection("users").Doc(userID)
-
-	// Create a reference to the "devices" subcollection within the user's document
-	devicesRef := userRef.Collection("devices")
-
-	// Store the received data in the "devices" subcollection
-	docRef, _, err := devicesRef.Add(context.Background(), data)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to store data"})
-		return
-	}
-
-	fmt.Println("Data stored in Firestore with ID:", docRef.ID)
-
-	c.JSON(http.StatusOK, gin.H{"message": "Data received successfully"})
+	r.Run()
 }
